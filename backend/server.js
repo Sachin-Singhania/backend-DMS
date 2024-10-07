2ad1d07f1d3058e4102f1abf2eb1c32fcc74bb65
Sachin Singhania <singhaniasachin160505@gmail.com>
2024-10-07 20:07:59 +0530

    Your commit message

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,8 @@ app.use(cookieParser());
 app.use(morgan('dev'))
 
 app.use(cors({
-  
- origin:"http://localhost:5173",
    credentials: true,
+ origin:"http://localhost:5173",
  }));
 
 let retryCount = 0;
